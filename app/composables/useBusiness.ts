ddc608fdf23204244382ce735f355e6683a9b6db
djruizz <dario.jruiz31@gmail.com>
2026-01-10 22:15:58 -0600

    Business Social Settigs adjustments

--- app/composables/useBusiness.ts
+++ app/composables/useBusiness.ts
@@ -1,46 +1,31 @@
 export const useBusiness = () => {
-  const client = useSupabaseClient();
-  const user = useSupabaseUser();
-
   const business = useState<Business | null>("business", () => null);
   const { setLoading } = useLoading();
 
   const fetchBusiness = async () => {
-    if (!user.value) return;
-    const { data, error } = await client
-      .from("business_profiles")
-      .select("*")
-      .eq("owner_id", user.value.sub)
-      .single();
-    if (error)
-      throw createError({
-        message: "Error al obtener la información del negocio",
-        status: 500,
-        statusMessage: error.message,
-      });
-    business.value = data;
-  };
-  const updateBusiness = async (data: BusinessInfoFormSchema) => {
-    if (!user.value) return;
-
     try {
       setLoading(true);
-      const { data: businessData, error } = await client
-        .from("business_profiles")
-        .update(data)
-        .eq("owner_id", user.value.sub)
-        .single();
-      if (error)
-        throw createError({
-          message: "Error al actualizar la información del negocio",
-          status: 500,
-          statusMessage: error.message,
-        });
-      business.value = businessData;
+      const res: Business = await $fetch("/api/business", {
+        method: "GET",
+      });
+      business.value = res;
     } finally {
       setLoading(false);
     }
   };
+  const updateBusiness = async (data: BusinessInfoFormSchema) => {
+    try {
+      setLoading(true);
+      const res: Business = await $fetch("/api/business", {
+        method: "PUT",
+        body: data,
+      });
+      business.value = res;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     business,
     fetchBusiness,
