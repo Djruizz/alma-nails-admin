c9f8df2b9a762fa00d2ae2e441598e154edc4c6f
djruizz <dario.jruiz31@gmail.com>
2026-01-25 17:53:54 -0600

    Closes #69 middleware cached data

--- app/middleware/is-owner.global.ts
+++ app/middleware/is-owner.global.ts
@@ -1,6 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const supabase = useSupabaseClient();
   const user = useSupabaseUser();
+  const userId = useState<string | null>("userId", () => null);
+  const businessCredentials = useState<{
+    id: string;
+    slug: string;
+  } | null>("businessCredentials", () => null);
+  const memberData = useState<any | null>("member", () => null);
 
   // Only apply strict permissions check for admin routes
   if (!to.path.includes("/admin")) {
@@ -20,41 +26,63 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   }
 
   const routeSlug = to.params.slug as string;
+  const siteIndex = `/business/${routeSlug}/site`;
+  // Retrieve existing business data check if matches current route
+  if (
+    !businessCredentials.value?.id ||
+    businessCredentials.value?.slug !== routeSlug
+  ) {
+    const { data: business, error: businessError } = await supabase
+      .from("business_profiles")
+      .select("id, slug")
+      .eq("slug", routeSlug)
+      .maybeSingle();
 
-  const { data: business, error: businessError } = await supabase
-    .from("business_profiles")
-    .select("id, slug")
-    .eq("slug", routeSlug)
-    .single();
+    // Redirect to /index if there is no existing business
+    if (businessError || !business) {
+      console.error("Error al obtener el negocio:");
+      return navigateTo(`/`);
+    }
 
-  // Redirect to /index if there is no existing business
-  if (businessError || !business) {
-    return navigateTo(`/`);
+    businessCredentials.value = {
+      id: business.id,
+      slug: business.slug,
+    };
+    // Reset member data as we changed business
+    memberData.value = null;
   }
 
   // Obtener el ID del usuario actual
-  const userId = user.value?.id || user.value?.sub;
+  userId.value = user.value?.id || user.value?.sub;
+
+  if (!userId.value) {
+    return navigateTo({
+      path: "/login",
+      query: { redirect: to.fullPath },
+    });
+  }
 
   // Verificar si el usuario es miembro del negocio
-  const { data: member, error: memberError } = await supabase
-    .from("business_members")
-    .select("role, business_id")
-    .eq("user_id", userId)
-    .eq("business_id", business.id)
-    .maybeSingle();
+  if (!memberData.value) {
+    const { data: member, error: memberError } = await supabase
+      .from("business_members")
+      .select("role, business_id")
+      .eq("user_id", userId.value)
+      .eq("business_id", businessCredentials.value?.id)
+      .maybeSingle();
 
-  const siteIndex = `/business/${routeSlug}/site`;
-
-  // Redirect to the site/index page if there is not permission
-  if (memberError || !member) {
-    console.error("Error al obtener el miembro:", memberError);
-    return navigateTo(siteIndex);
+    // Redirect to the site/index page if there is not permission
+    if (memberError || !member) {
+      console.error("Error al obtener el miembro:", memberError);
+      return navigateTo(siteIndex);
+    }
+    memberData.value = member;
   }
 
   const allowedRoles = ["owner"];
 
   // Add the roles permitted, by now only "owner"
-  if (!allowedRoles.includes(member.role || "")) {
+  if (!allowedRoles.includes(memberData.value.role || "")) {
     console.error("El usuario no tiene permisos suficientes");
     return navigateTo(siteIndex);
   }
