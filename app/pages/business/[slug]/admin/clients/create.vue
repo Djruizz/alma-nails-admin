3cc82ea2f35a84e7cf6a90fdbc087b4c570c2c8e
Djruizz <dario.jruiz31@gmail.com>
2026-02-03 12:38:44 -0600

    Initial state of create client page

---
+++ app/pages/business/[slug]/admin/clients/create.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "admin",
+});
+</script>
+
+<template>
+  <UContainer class="py-6">
+    <div>
+      <h1 class="text-3xl font-bold text-gray-900 dark:text-white mb-2">
+        Crear Cliente
+      </h1>
+      <p class="text-gray-500">Crea un nuevo cliente</p>
+    </div>
+    <UForm>
+      <UFormField label="Nombre">
+        <UInput placeholder="Nombre" />
+      </UFormField>
+      <UFormField label="Apellido">
+        <UInput placeholder="Apellido" />
+      </UFormField>
+      <UFormField label="Email">
+        <UInput placeholder="Email" />
+      </UFormField>
+      <UFormField label="Telefono">
+        <UInput placeholder="Telefono" />
+      </UFormField>
+    </UForm>
+  </UContainer>
+</template>
