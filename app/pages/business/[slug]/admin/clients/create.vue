ca005f2d95d120e779ca91ea5843646ccff66a1b
djruizz <dario.jruiz31@gmail.com>
2026-02-05 20:02:08 -0600

    Create client form added

--- app/pages/business/[slug]/admin/clients/create.vue
+++ app/pages/business/[slug]/admin/clients/create.vue
@@ -10,21 +10,28 @@ definePageMeta({
       <h1 class="text-3xl font-bold text-gray-900 dark:text-white mb-2">
         Crear Cliente
       </h1>
-      <p class="text-gray-500">Crea un nuevo cliente</p>
+      <p class="text-gray-500">Crea un nuevo cliente sin registro</p>
     </div>
-    <UForm>
-      <UFormField label="Nombre">
-        <UInput placeholder="Nombre" />
-      </UFormField>
-      <UFormField label="Apellido">
-        <UInput placeholder="Apellido" />
-      </UFormField>
-      <UFormField label="Email">
-        <UInput placeholder="Email" />
-      </UFormField>
-      <UFormField label="Telefono">
-        <UInput placeholder="Telefono" />
-      </UFormField>
-    </UForm>
+    <UCard class="mt-6">
+      <UForm class="space-y-4">
+        <UFormField label="Nombre Completo" class="w-full" required>
+          <UInput placeholder="Nombre" class="w-full" />
+        </UFormField>
+        <UFormField label="Telefono" class="w-full" required>
+          <UInput placeholder="3312345678" class="w-full" />
+        </UFormField>
+        <UFormField label="Notas" class="w-full">
+          <UTextarea placeholder="Notas opcionales" class="w-full" />
+        </UFormField>
+        <UiFormActionButtons
+          class="pt-4"
+          submit-label="Crear Cliente"
+          :canceling="false"
+          :hasChanges="false"
+          @reset="false"
+          @submit="false"
+        />
+      </UForm>
+    </UCard>
   </UContainer>
 </template>
